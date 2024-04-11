201be35220082fb0fff136d4a10e94873ba743f9
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-04-11 09:00:14 -0300

    Creo Factories y aplico inyeccion de dependencias
    (cherry picked from commit 47efa2aacd02baea39ca7f83882a7fd00a716490)

--- app/src/main/java/ayds/songinfo/home/view/HomeViewInjector.kt
+++ app/src/main/java/ayds/songinfo/home/view/HomeViewInjector.kt
@@ -5,7 +5,7 @@ import ayds.songinfo.home.model.HomeModelInjector
 
 object HomeViewInjector {
 
-    val songDescriptionHelper: SongDescriptionHelper = SongDescriptionHelperImpl()
+    val songDescriptionHelper: SongDescriptionHelper = SongDescriptionHelperImpl(SongReleaseDateFactoryImpl())
 
     fun init(homeView: HomeView) {
         HomeModelInjector.initHomeModel(homeView)
